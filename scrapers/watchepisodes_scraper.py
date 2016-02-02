9532e7dd5649a28591fe70ed2115b54fe93ade56
tknorris <tknorris@tknorris-laptop>
2016-02-02 01:35:39 -0500

    update watchep episode matching

--- scrapers/watchepisodes_scraper.py
+++ scrapers/watchepisodes_scraper.py
@@ -91,8 +91,34 @@ class WatchEpisodes_Scraper(scraper.Scraper):
         return self._default_get_url(video)
 
     def _get_episode_url(self, show_url, video):
-        episode_pattern = 'href="([^"]*-[sS]%02d[eE]%02d(?!\d)[^"]*)' % (int(video.season), int(video.episode))
-        return self._default_get_episode_url(show_url, video, episode_pattern)
+        url = urlparse.urljoin(self.base_url, show_url)
+        html = self._http_get(url, cache_limit=2)
+        if html:
+            force_title = scraper_utils.force_title(video)
+            episodes = dom_parser.parse_dom(html, 'div', {'class': '\s*el-item\s*'})
+            if not force_title:
+                episode_pattern = 'href="([^"]*-[sS]%02d[eE]%02d(?!\d)[^"]*)' % (int(video.season), int(video.episode))
+                match = re.search(episode_pattern, html)
+                if match:
+                    return scraper_utils.pathify_url(match.group(1))
+                
+                if kodi.get_setting('airdate-fallback') == 'true' and video.ep_airdate:
+                    airdate_pattern = '%02d-%02d-%d' % (video.ep_airdate.day, video.ep_airdate.month, video.ep_airdate.year)
+                    for episode in episodes:
+                        ep_url = dom_parser.parse_dom(episode, 'a', ret='href')
+                        ep_airdate = dom_parser.parse_dom(episode, 'div', {'class': 'date'})
+                        if ep_url and ep_airdate:
+                            ep_airdate = ep_airdate[0].strip()
+                            if airdate_pattern == ep_airdate:
+                                return scraper_utils.pathify_url(ep_url[0])
+
+            if (force_title or kodi.get_setting('title-fallback') == 'true') and video.ep_title:
+                norm_title = scraper_utils.normalize_title(video.ep_title)
+                for episode in episodes:
+                    ep_url = dom_parser.parse_dom(episode, 'a', ret='href')
+                    ep_title = dom_parser.parse_dom(episode, 'div', {'class': 'e-name'})
+                    if ep_url and ep_title and norm_title == scraper_utils.normalize_title(ep_title[0]):
+                        return scraper_utils.pathify_url(ep_url[0])
 
     def search(self, video_type, title, year):
         results = []
