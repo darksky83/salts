34434561e1cce9790487001ea3346724804f1097
tknorris <tknorris@tknorris-laptop>
2016-03-04 15:45:49 -0500

    remove single quotes from search title for furk

--- scrapers/furk_scraper.py
+++ scrapers/furk_scraper.py
@@ -83,7 +83,7 @@ class Furk_Scraper(scraper.Scraper):
         if source_url and source_url != FORCE_NO_MATCH:
             params = urlparse.parse_qs(urlparse.urlparse(source_url).query)
             if 'title' in params:
-                query = params['title'][0]
+                query = params['title'][0].replace("'", "")
                 if video.video_type == VIDEO_TYPES.MOVIE:
                     if 'year' in params: query += ' %s' % (params['year'][0])
                 else:
