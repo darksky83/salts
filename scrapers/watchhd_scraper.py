b83194c9882f9afeff70afa4be24ff75032f5b34
tknorris <tknorris@tknorris-laptop>
2016-03-08 18:24:36 -0500

    improve url fix

--- scrapers/watchhd_scraper.py
+++ scrapers/watchhd_scraper.py
@@ -128,6 +128,9 @@ class WatchHD_Scraper(scraper.Scraper):
             url = urlparse.urljoin(self.base_url, source_url)
             url = url.replace('/film/', '/filminfo/')
             html = self._http_get(url, cache_limit=.5)
+            if not html:
+                url = url.replace('/film/', '/filminfo/')
+                html = self._http_get(url, cache_limit=.5)
             match = re.search('<b>Views:.*?([\d,]+)', html)
             if match:
                 views = int(match.group(1).replace(',', ''))
@@ -171,8 +174,10 @@ class WatchHD_Scraper(scraper.Scraper):
 
     def _get_episode_url(self, season_url, video):
         url = urlparse.urljoin(self.base_url, season_url)
-        url = url.replace('/film/', '/filminfo/')
         html = self._http_get(url, cache_limit=2)
+        if not html:
+            url = url.replace('/film/', '/filminfo/')
+            html = self._http_get(url, cache_limit=.5)
         html = self.__get_watch_now(html)
         match = re.search('<a[^>]+id="ep_\d+"[^>]*>\s*%s\s*<' % (video.episode), html)
         if match:
